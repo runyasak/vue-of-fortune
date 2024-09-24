5a289192b7407eee21ebd036a90859d0c999969b
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-09-24 20:12:42 +0700

    update antfu eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
   overrides: [
     {
       files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
+        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}',
       ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
+      extends: [
+        'plugin:playwright/recommended',
+      ],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
