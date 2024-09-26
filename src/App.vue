ece1c410710515539ae15f89d9b1b14356ba918a
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-09-27 01:06:13 +0700

    implement attendee.json

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 import { useIntervalFn } from '@vueuse/core'
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
+import attendeeList from './attendee.json'
 import RandomLetter from './RandomLetter.vue'
 
 const codeLength = 8
 
-const winnerCode = 'M9CW2LEJ'
+const winnerAttendee = ref<{ code: string, name: string } | null>(null)
+
+const previewWinnerName = ref<string | null>(null)
 
 const initialStopLetters = ['G', 'O', 'O', 'D', 'L', 'U', 'C', 'K']
 
 const stopLetters = ref<(string | null)[]>(initialStopLetters)
 
-const previewWinnerName = ref<string | null>(null)
-
 const countdownIndex = ref(0)
 
 const randomActive = ref(false)
 
+const winnerCode = computed(() => winnerAttendee.value?.code || '')
+
 const countdownInterval = useIntervalFn(() => {
-  stopLetters.value[countdownIndex.value] = winnerCode[countdownIndex.value]
+  stopLetters.value[countdownIndex.value] = winnerCode.value[countdownIndex.value]
   countdownIndex.value++
 }, 500, { immediate: false, immediateCallback: true })
 
@@ -28,18 +31,26 @@ watch(countdownIndex, (value) => {
     randomActive.value = false
     countdownIndex.value = 0
     countdownInterval.pause()
-    previewWinnerName.value = 'Runyasak Chaengnaimuang'
+    previewWinnerName.value = winnerAttendee.value?.name || ''
   }
 })
 
 function clickStart() {
   randomActive.value = true
   previewWinnerName.value = null
-  stopLetters.value = Array.from({ length: initialStopLetters.length }, () => null)
+  winnerAttendee.value = null
+  stopLetters.value = Array.from({ length: codeLength }, () => null)
 }
 
 function clickStop() {
   if (!countdownInterval.isActive.value) {
+    const randomAttendee = attendeeList[Math.ceil(Math.random() * attendeeList.length - 1)]
+
+    winnerAttendee.value = {
+      code: randomAttendee.code,
+      name: randomAttendee.name,
+    }
+
     countdownInterval.resume()
   }
 }
@@ -53,34 +64,34 @@ function clickReset() {
 </script>
 
 <template>
-  <div class="container mx-auto flex h-screen flex-col items-center justify-center p-4">
+  <div class="container mx-auto flex h-screen flex-col items-center justify-center gap-24 p-4">
     <div class="relative">
-      <div v-if="previewWinnerName" class="animate__animated animate__fadeInDown absolute bottom-24 w-full text-center text-4xl">
+      <div v-if="previewWinnerName" class="animate__animated animate__fadeInDown absolute bottom-48 w-full text-center text-4xl lg:bottom-24">
         {{ previewWinnerName }}
       </div>
-      <div class="flex gap-16">
+      <div class="flex max-w-md flex-wrap items-center justify-center gap-16 lg:max-w-full">
         <RandomLetter v-for="(letter, index) in stopLetters" :key="index" :stop-letter="letter" />
       </div>
     </div>
 
-    <div class="flex gap-8 pt-24">
+    <div class="flex gap-16">
       <button
         v-if="randomActive"
-        class="rounded-lg border-2 border-primary p-2 transition-colors ease-in-out hover:bg-primary [&_svg]:hover:!text-white"
+        class="rounded-lg border-2 border-primary p-1.5 transition-colors ease-in-out hover:bg-primary [&_svg]:hover:!text-white"
         @click="clickStop"
       >
         <Icon class="text-2xl transition-colors ease-in-out" icon="solar:pause-bold" color="#44bd87" />
       </button>
       <button
         v-else
-        class="rounded-lg border-2 border-primary p-2 transition-colors ease-in-out hover:bg-primary [&_svg]:hover:!text-white"
+        class="rounded-lg border-2 border-primary p-1.5 transition-colors ease-in-out hover:bg-primary [&_svg]:hover:!text-white"
         @click="clickStart"
       >
         <Icon class="text-2xl transition-colors ease-in-out" icon="solar:play-bold" color="#44bd87" />
       </button>
 
       <button
-        class="rounded-lg border-2 border-white p-2 transition-colors ease-in-out hover:bg-white [&_svg]:hover:!text-black"
+        class="rounded-lg border-2 border-white p-1.5 transition-colors ease-in-out hover:bg-white [&_svg]:hover:!text-black"
         @click="clickReset"
       >
         <Icon class="text-2xl transition-colors ease-in-out" icon="solar:restart-bold" color="white" />
