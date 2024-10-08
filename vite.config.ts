741481241965da415970ba506739c71282c8e463
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-10-08 14:04:21 +0700

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://runyasak.github.io/vue-of-fortune/',
   plugins: [
     vue(),
     vueDevTools(),
